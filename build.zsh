5a2ce916d996f27c8d60101af89931fa5773c475
Rommel M. Martinez <ebzzry@gmail.com>
2015-08-17 04:52:59 +0800

    Replaced the zsh build script with a bash one, due to portability issues

--- build.zsh
+++
@@ -1,8 +0,0 @@
-# -*- mode: sh -*-
-
-export PATH=$PATH:$HOME/bin
-
-mkdir stage > /dev/null 2>&1
-for i (*.md~FOOTER.md~README.md~TODO.md) cat $i FOOTER.md >! stage/$i
-for i (stage/*.md) emem -Ro ${i:t:r}.html $i
-rm -rf stage > /dev/null 2>&1
