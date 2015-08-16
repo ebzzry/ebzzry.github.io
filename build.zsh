37efef91927347189ab005fff24016d77583568b
Rommel Mᴀʀᴛɪɴᴇᴢ <ebzzry@gmail.com>
2015-08-16 23:38:38 +0800

    Updated build.zsh

--- build.zsh
+++ build.zsh
@@ -2,7 +2,7 @@
 
 export PATH=$PATH:$HOME/bin
 
-mkdir stage
+mkdir stage > /dev/null 2>&1
 for i (*.md~FOOTER.md~README.md~TODO.md) cat $i FOOTER.md >! stage/$i
 for i (stage/*.md) emem -Ro ${i:t:r}.html $i
-rm -rf stage
+rm -rf stage > /dev/null 2>&1
